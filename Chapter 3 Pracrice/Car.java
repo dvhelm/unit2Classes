98a524bf253cc61650a02ad9ce469e834db8204f
dvhelm <dvhelm@NN123-HA-15.HS.sd203.org>
2015-09-11 14:14:45 -0500

    Began Using Debugger

--- Chapter 3 Pracrice/Car.java
+++ Chapter 3 Pracrice/Car.java
@@ -1,5 +1,4 @@
 
-
 /**
  * A car has a certain fuel efficiency and a certain amount of fuel in the gas tank.
  *  The car may be driven which reduces the amount of gas in the fuel tank.
@@ -8,19 +7,21 @@
  */
 public class Car
 {
-    /** the fuel efficiency for the car measured in units of miles / gallon (mpg) */
+    /** the fuel efficiency for the car measured in units of miles / gallon (mpg) 
+     */
     private double fuelEfficiency;
-    
-    /** The amount of fuel in the tank of the car measured in units of gallons */
+
+    /** The amount of fuel in the tank of the car measured in units of gallons
+     */
     private double fuelInTank;
-    
-    
+
+   
     /**
      * Constructor for objects of class Car that specifies the fuel efficiency
      */
     public Car( double fuelEfficiency )
     {
-        fuelEfficiency = fuelEffciency;
+        fuelEfficiency = fuelEfficiency;
         fuelInTank = 0;
     }
 
@@ -31,10 +32,29 @@ public class Car
      * @pre        the specified distance will not consume more than the available gas
      * @param    distance      the specified distance to drive in units of miles
      */
-    public void drive( double distance );
-    public int sampleMethod(int y)
+
+    public void drive(double distance)
     {
-        
+        fuelInTank -= distance / fuelEfficiency;
     }
 
+    /**
+     * Returns the number of gallons of gas in the car's tank
+     * @return   the number of gallons of gas in teh car's tank
+     */
+    public double getGasInTank()
+    {
+        return fuelInTank;
+    }
+
+    /**
+     * Increments the amount of gas in the car's tank by the specified number of gallons.
+     *  
+     * @pre        gallonsOfGas must be positive
+     * @param    gallonsOfGas      amount of gas to add to the car's tank
+     */
+    public void addGas(double gallonsOfGas)
+    {
+        fuelInTank += gallonsOfGas;
+    }
 }
