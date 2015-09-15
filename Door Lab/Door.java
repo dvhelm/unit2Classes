c975fd612979438d0682d363639b750ff7735f8e
dvhelm <dvhelm@NN123-HA-15.HS.sd203.org>
2015-09-15 14:10:56 -0500

    Did Door Lab Kind of

---
+++ Door Lab/Door.java
@@ -0,0 +1,137 @@
+/**
+ * An example of a method - replace this comment with your own
+ *  that describes the operation of the method
+ *
+ * @pre     preconditions for the method
+ *          (what the method assumes about the method's parameters and class's state)
+ * @post    postconditions for the method
+ *          (what the method guarantees upon completion)
+ * @param   y   description of parameter y
+ * @return  description of the return value
+ */
+public class Door
+{
+
+    private String nameOfDoor;
+     
+    private String stateOfDoor;
+
+    /**
+     * An example of a method - replace this comment with your own
+     *  that describes the operation of the method
+     *
+     * @pre     preconditions for the method
+     *          (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *          (what the method guarantees upon completion)
+     * @param   y   description of parameter y
+     * @return  description of the return value
+     */
+    public Door(String stateOfDoor,String nameOfDoor)
+    {
+        this.nameOfDoor=nameOfDoor;
+        this.stateOfDoor=stateOfDoor;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *  that describes the operation of the method
+     *
+     * @pre     preconditions for the method
+     *          (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *          (what the method guarantees upon completion)
+     * @param   y   description of parameter y
+     * @return  description of the return value
+     */
+    public void open()
+    {
+        this.stateOfDoor="open";
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *  that describes the operation of the method
+     *
+     * @pre     preconditions for the method
+     *          (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *          (what the method guarantees upon completion)
+     * @param   y   description of parameter y
+     * @return  description of the return value
+     */
+    public void close()
+    {
+        this.stateOfDoor="close";
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *  that describes the operation of the method
+     *
+     * @pre     preconditions for the method
+     *          (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *          (what the method guarantees upon completion)
+     * @param   y   description of parameter y
+     * @return  description of the return value
+     */
+    public String getNameOfDoor()
+    {
+        // put your code here
+        return this.nameOfDoor;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *  that describes the operation of the method
+     *
+     * @pre     preconditions for the method
+     *          (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *          (what the method guarantees upon completion)
+     * @param   y   description of parameter y
+     * @return  description of the return value
+     */
+    public String getStateofDoor()
+    {
+        // put your code here
+        return this.stateOfDoor;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *  that describes the operation of the method
+     *
+     * @pre     preconditions for the method
+     *          (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *          (what the method guarantees upon completion)
+     * @param   y   description of parameter y
+     * @return  description of the return value
+     */
+    public void setName(String newName)
+    {
+        // put your code here
+        this.nameOfDoor = newName;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *  that describes the operation of the method
+     *
+     * @pre     preconditions for the method
+     *          (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *          (what the method guarantees upon completion)
+     * @param   y   description of parameter y
+     * @return  description of the return value
+     */
+    public void setState(String newState)
+    {
+        // put your code here
+        this.stateOfDoor = newState;
+    }
+
+}
+
