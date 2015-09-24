92ecd7d79a57270c3db8db96897cac0340cc0404
dvhelm <dvhelm@NN123-HA-15.HS.sd203.org>
2015-09-24 14:14:49 -0500

    Started Chapter 4

---
+++ AvgOfThree/DistanceConverter.java
@@ -0,0 +1,13 @@
+public class DistanceConverter
+{
+      public static final double YARDS = 3.5;
+      public static final double FEET = YARDS * 3;
+      public static final double INCHES = FEET * 12;
+   public static void main(String[] args)
+   {
+
+      
+      System.out.println(YARDS + " yards are " + FEET + " feet");
+      System.out.println(YARDS + " yards are " + INCHES + " inches");
+   }
+}
