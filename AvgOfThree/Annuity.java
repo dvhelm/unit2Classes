de7ecafc4d31421f1d7250eafde71156698c3b98
dvhelm <dvhelm@NN123-HA-15.HS.sd203.org>
2015-09-28 14:14:31 -0500

    Finished Annuity

--- AvgOfThree/Annuity.java
+++ AvgOfThree/Annuity.java
@@ -7,5 +7,14 @@ public class Annuity
         double PVann = 10000;
         double x = 1;
         double upper = x+i;
-        double upper = Math.static double pow(upper,n-x);
-        
+        double exponent= Math.pow (upper, n-x);
+        double upper2= exponent-x;
+        double uppert = upper2/i;
+        double lower= x+i;
+        double lexponent = Math.pow(lower,n-x);
+        double stotal = uppert/lexponent;
+        double total = stotal+1;
+        double PMT = PVann/ total;
+        System.out.println(PMT);
+    }
+}
